0e1eec6ef8d7072282e05337d3f7013a385692df
MuhammadMuaazMk <166921901+MuhammadMuaazMk@users.noreply.github.com>
2024-11-20 14:13:09 +0500

    GSAP Project

---
+++ src/App.jsx
@@ -0,0 +1,141 @@
+import "./index.css";
+import Canvas from "./Canvas";
+import data from "./data";
+import LocomotiveScroll from "locomotive-scroll";
+import { useEffect, useState, useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { Circ, Expo } from "gsap/all";
+
+function App() {
+  const [showCanvas, setShowCanvas] = useState(false);
+  const headingref = useRef(null);
+  const growingSpan = useRef(null);
+
+  useEffect(() => {
+    const locomotiveScroll = new LocomotiveScroll();
+  }, []);
+
+  useEffect(() => {
+    const handleClick = (e) => {
+      setShowCanvas((prevShowCanvas) => {
+        if (!prevShowCanvas) {
+          gsap.set(growingSpan.current, {
+            top: e.clientY,
+            left: e.clientX,
+          });
+
+          gsap.to("body", {
+            color: "#000",
+            backgroundColor: "#fd2c2a",
+            duration: 1.2,
+            ease: "power2.inOut",
+          });
+
+          gsap.to(growingSpan.current, {
+            scale: 1000,
+            duration: 2,
+            ease: "power2.inOut",
+            onComplete: () => {
+              gsap.set(growingSpan.current, {
+                scale: 0,
+                clearProps: "all",
+              });
+            },
+          });
+        } else {
+          gsap.to("body", {
+            color: "#fff",
+            backgroundColor: "#000",
+            duration: 1.2,
+            ease: "power2.inOut",
+          });
+        }
+
+        return !prevShowCanvas;
+      });
+    };
+
+    const headingElement = headingref.current;
+    headingElement.addEventListener("click", handleClick);
+
+    // Clean up event listener on unmount
+    return () => headingElement.removeEventListener("click", handleClick);
+  }, []);
+
+  return (
+    <>
+      <span
+        ref={growingSpan}
+        className="growing rounded-full block fixed top-[-20px] left-[-20px] w-5 h-5"
+      ></span>
+      <div className="w-full relative min-h-screen font-['Helvetica_Now_Display']">
+        {showCanvas &&
+          data[0].map((canvasdets, index) => <Canvas details={canvasdets} />)}
+        <div className="w-full relative z-[1] h-screen ">
+          <nav className="w-full p-8 flex justify-between z-50">
+            <div className="brand text-5xl tracking-wider font-md font-cookie">Thirtysixstudios</div>
+            <div className="links flex gap-6 font-pop font-light text-2xl    ">
+              {[
+                "What we do",
+                "Who we are",
+                "How we give back",
+                "Talk to us",
+              ].map((link, index) => (
+                <a
+                  key={index}
+                  href={`#${link.toLowerCase()}`}
+                  className="text-md hover:text-gray-300 px-3 py-3"
+                >
+                  {link}
+                </a>
+              ))}
+            </div>
+          </nav>
+          <div className="textcontainer  w-full px-[20%]">
+            <div className="text w-[50%]">
+              <h3 className="text-4xl leading-[1.2] font-pop">
+                At Thirtysixstudio, we build immersive digital experiences for
+                brands with a purpose.
+              </h3>
+              <p className=" w-[99%] text-2xl mt-6 font-extralight font-pop">
+                We are a team of designers, developers, and strategists who are
+                passionate about creating digital experiences that are both
+                beautiful and functional.
+              </p>
+              <p className="text-5xl mt-6 font-cookie">Scroll</p>
+            </div>
+          </div>
+          <div className="w-full absolute bottom-0 left-0">
+            <h1
+              ref={headingref}
+              className="text-[21rem] font-normal tracking-wide leading-none pl-5"
+            >
+              Thirtysixstudios
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div className="w-full relative h-screen  mt-40 px-10">
+        {showCanvas &&
+          data[1].map((canvasdets, index) => <Canvas details={canvasdets} />)}
+        <h1 className="text-8xl tracking-tighter font-pop ">About the brand</h1>
+        <p className="text-3xl leading-[1.8] w-[50%] mt-10 font-extralight font-pop ">
+          We are a team of designers, developers, and strategists who are
+          passionate about creating digital experiences that are both beautiful
+          and functional, we are a team of designers, developers, and
+          strategists who are passionate about creating digital experiences that
+          are both beautiful and functional.
+        </p>
+
+        <img
+          className="w-[46%] mt-10 ml-[1190px] rounded-lg shadow-2xl shadow-slate-800"
+          src="https://img.freepik.com/premium-photo/red-hot-chili-pepper-with-drops-water-dark-background_1152200-69.jpg"
+          alt=""
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
